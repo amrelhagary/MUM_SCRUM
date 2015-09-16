484e2fdd9bd941aab8f6e7851f6a1cc92ae54e1a
Amr Elhagary <amr.elhagary@gmail.com>
2015-09-16 00:45:35 -0500

    remove unnecessary files and fix path url

--- MUMScrum/WebContent/assets/app/employee/employee.factory.js
+++ MUMScrum/WebContent/assets/app/employee/employee.factory.js
@@ -2,13 +2,13 @@ angular
 	.module('scrumApp.employee')
 	.factory('EmployeeFactory', ['$resource',
 		  function($resource){
-		    return $resource('http://localhost:8181/MUMScrum/API/EmployeeControllerWS/employee/:id', {}, {
+		    return $resource('http://localhost:8085/MUMScrum/API/EmployeeControllerWS/employee/:id', {}, {
 		      'query': { method:'GET',isArray:false },
 		      'update': { method: 'PUT' }
 		    });
   }])
 	.factory('EmployeeRole',['$resource',function($resource){
-		return $resource('http://localhost:8181/MUMScrum/API/RoleControllerWS/role',{},{
+		return $resource('http://localhost:8085/MUMScrum/API/RoleControllerWS/role',{},{
 			'query': {method: 'GET', isArray: false }
 		})
 	}])
