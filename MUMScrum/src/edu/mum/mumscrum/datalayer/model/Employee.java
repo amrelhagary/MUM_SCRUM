422f91deb83406f60a999740b0bc2a2fd9df108e
3ouri <abdallah.ouri@hotmail.com>
2015-09-16 14:30:30 -0500

    updated employee entity

--- MUMScrum/src/edu/mum/mumscrum/datalayer/model/Employee.java
+++ MUMScrum/src/edu/mum/mumscrum/datalayer/model/Employee.java
@@ -4,12 +4,9 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 
@@ -39,9 +36,8 @@ public class Employee implements Serializable {
 
 	private String phone;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ROLE_ID")
-	private Role role;
+	@Column(name = "ROLE_ID")
+	private long roleId;
 
 	private String username;
 
@@ -104,11 +100,11 @@ public class Employee implements Serializable {
 		this.username = username;
 	}
 
-	public Role getRole() {
-		return this.role;
+	public long getRoleId() {
+		return roleId;
 	}
 
-	public void setRole(Role role) {
-		this.role = role;
+	public void setRoleId(long roleId) {
+		this.roleId = roleId;
 	}
 }
