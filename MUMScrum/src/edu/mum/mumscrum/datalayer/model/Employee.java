42807e4147cccff9f4e9f2f1cd951540d77280fc
3ouri <abdallah.ouri@hotmail.com>
2015-09-16 11:54:04 -0500

    Updated datalayer

--- MUMScrum/src/edu/mum/mumscrum/datalayer/model/Employee.java
+++ MUMScrum/src/edu/mum/mumscrum/datalayer/model/Employee.java
@@ -4,9 +4,12 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 
@@ -36,8 +39,9 @@ public class Employee implements Serializable {
 
 	private String phone;
 
-	@Column(name = "ROLE_ID")
-	private long roleId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ROLE_ID")
+	private Role role;
 
 	private String username;
 
@@ -92,14 +96,6 @@ public class Employee implements Serializable {
 		this.phone = phone;
 	}
 
-	public long getRoleId() {
-		return this.roleId;
-	}
-
-	public void setRoleId(long roleId) {
-		this.roleId = roleId;
-	}
-
 	public String getUsername() {
 		return this.username;
 	}
@@ -108,4 +104,11 @@ public class Employee implements Serializable {
 		this.username = username;
 	}
 
+	public Role getRole() {
+		return this.role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 }
--- MUMScrum/src/edu/mum/mumscrum/datalayer/model/Role.java
+++ MUMScrum/src/edu/mum/mumscrum/datalayer/model/Role.java
@@ -3,19 +3,18 @@ package edu.mum.mumscrum.datalayer.model;
 import java.io.Serializable;
 import javax.persistence.*;
 
-
 /**
  * The persistent class for the "ROLE" database table.
  * 
  */
 @Entity
-@Table(name="\"ROLE\"")
-@NamedQuery(name="Role.findAll", query="SELECT r FROM Role r")
+@Table(name = "\"ROLE\"")
+@NamedQuery(name = "Role.findAll", query = "SELECT r FROM Role r")
 public class Role implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name="ROLE_ID")
+	@Column(name = "ROLE_ID")
 	private long roleId;
 
 	private String name;
