aac6fd33dbb0f7f69a928ed6688cfd4ab2b491f2
asem alnajjar <asemalnajar@yahoo.com>
2015-09-20 19:15:07 -0500

    Release WS / Get

--- MUMScrum/src/edu/mum/mumscrum/controller/ReleaseController.java
+++ MUMScrum/src/edu/mum/mumscrum/controller/ReleaseController.java
@@ -1,8 +1,62 @@
 package edu.mum.mumscrum.controller;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.JsonObject;
+
+import edu.mum.mumscrum.datalayer.model.Release;
+import edu.mum.mumscrum.service.ReleaseService;
+import edu.mum.mumscrum.utility.MUMScrumUtil;
 
 @Path("ReleaseControllerWS")
 public class ReleaseController {
 
+	private ReleaseService releaseservice;
+	
+	public ReleaseController() {
+	
+		releaseservice =  new ReleaseService();
+	}
+	
+	@GET
+	@Path("/release")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAllReleases()
+	{
+		List<Release> releaselist = releaseservice.getAllReleases();
+		JsonObject result = MUMScrumUtil.prepareJsonObjectResponse(releaselist);
+		return Response.status(200).entity(result.toString()).build();
+	}
+	
+	@GET
+	@Path("/release/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getReleaseById(@PathParam("id" )String id )
+	{
+		Release release = releaseservice.getReleaseById(id);
+		JsonObject result  = MUMScrumUtil.prepareJsonObjectResponse(release);
+	     return Response.status(200).entity(result.toString()).build();
+	}
+	
+	@POST
+	@Path("/release")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addRelease( Release release)
+	{
+		Release rls = releaseservice.updateRelease(release);
+		JsonObject result = MUMScrumUtil.prepareJsonObjectResponse(rls);
+		return Response.status(200).entity(result.toString()).build();
+	}
+	
 }
--- MUMScrum/src/edu/mum/mumscrum/datalayer/dao/ReleaseDAO.java
+++ MUMScrum/src/edu/mum/mumscrum/datalayer/dao/ReleaseDAO.java
@@ -1,5 +1,13 @@
 package edu.mum.mumscrum.datalayer.dao;
 
+import java.util.List;
+
+import org.eclipse.persistence.expressions.Expression;
+import org.eclipse.persistence.expressions.ExpressionBuilder;
+
+import edu.mum.mumscrum.common.ConfigurationConstants.SortingType;
+import edu.mum.mumscrum.datalayer.model.Release;
+
 public class ReleaseDAO {
 	
 	private static ReleaseDAO releaseDAO;
@@ -16,4 +24,36 @@ public class ReleaseDAO {
 		return releaseDAO;
 	}
 
+	public List<Release> getAllReleases()
+	{
+		return mumScrumDAO.getAllObjectsByExpression(Release.class, new ExpressionBuilder(), 
+				SortingType.ASCENDING ,"id");	
+	}
+	
+	
+	public Release getReleaseById(String id)
+	{
+		Expression exp = new ExpressionBuilder().get("id").equal(id);
+		return mumScrumDAO.getObjectByExpression(Release.class, exp);
+	}
+	public Release addRelease(Release release)
+	{
+		return mumScrumDAO.addObject(release);
+	}
+	
+	public Release updateRelease (Release release )
+	{
+
+		return mumScrumDAO.updateObject(release);
+	}
+	
+	public Release deleteRelease(Release release)
+	{
+		return mumScrumDAO.deleteObject(release);
+	}
+	public List<Release> deleteReleaseById(String id)
+	{
+		Expression exp = new ExpressionBuilder().get("id").equal(id);
+		return mumScrumDAO.deleteAllObjectsBasedOnExpression(Release.class, exp);
+	}
 }
