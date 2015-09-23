b2b5bb3cb68feab48bbc3968e406826d66e08f9e
Amr Elhagary <amr.elhagary@gmail.com>
2015-09-22 23:17:15 -0500

    validate required fields login form

--- MUMScrum/WebContent/assets/app/authentication/auth.js
+++ MUMScrum/WebContent/assets/app/authentication/auth.js
@@ -16,16 +16,21 @@ angular
 .controller('LoginCtrl',['$scope','AuthService','$location',function($scope,AuthService,$location){
 
 	AuthService.ClearCredentials();
-	$scope.login = function(){
-		AuthService.login($scope.username,$scope.password,function(response){
-			if(response.success)
-			{
-				AuthService.setCredentials(response.username,response.password,response.role);
-				$location.path(response.home_route);
-			}else{
-				$scope.message = response.message;
-			}
-		});
+	$scope.login = function(isValid){
+
+		if(isValid)
+		{
+			AuthService.login($scope.username,$scope.password,function(response){
+				if(response.success)
+				{
+					AuthService.setCredentials(response.username,response.password,response.role);
+					$location.path(response.home_route);
+				}else{
+					$scope.message = response.message;
+				}
+			});
+		}
+
 	}
 }])
 .controller('LogoutCtrl', ['AuthService','$location', function(AuthService,$location){
