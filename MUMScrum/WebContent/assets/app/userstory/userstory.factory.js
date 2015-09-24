4bf2eb14ac61785765dcf3a307f3155091c8843b
Khaled Saleh <khaledacmilan@gmail.com>
2015-09-23 23:05:01 -0600

    assign user stories list
    - add userstory details page

--- MUMScrum/WebContent/assets/app/userstory/userstory.factory.js
+++ MUMScrum/WebContent/assets/app/userstory/userstory.factory.js
@@ -14,4 +14,20 @@ angular
 		    return $resource('http://localhost:8085/MUMScrum/API/UserStoryControllerWS/product/:productId/userstory', {}, {
 		      'query': { method:'GET',isArray:false }
 		    });
+  }])
+	.factory('GetAllUserStoryByUserId', ['$resource',
+		  function($resource){
+		    return $resource('http://localhost:8085/MUMScrum/API/UserStoryControllerWS/assignee/:UserId/userstory', {}, {
+		      'query': { method:'GET',isArray:false }
+		    });
+  }])
+	.factory('UserStoryGetById', ['$resource',
+		  function($resource){
+		    return $resource('http://localhost:8085/MUMScrum/API/UserStoryControllerWS/userstory/:id', {}, {
+		      'query': { method:'GET',isArray:false },
+		      'update': { method: 'PUT' }
+		    });
   }]);
+
+
+	
