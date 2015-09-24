9b711d1dee56144557147bd37480e7303bee4614
Amr Elhagary <amr.elhagary@gmail.com>
2015-09-23 21:59:49 -0500

    get userstory assigned to sprint or nonsprint

--- MUMScrum/WebContent/assets/app/userstory/userstory.factory.js
+++ MUMScrum/WebContent/assets/app/userstory/userstory.factory.js
@@ -14,4 +14,16 @@ angular
 		    return $resource('http://localhost:8085/MUMScrum/API/UserStoryControllerWS/product/:productId/userstory', {}, {
 		      'query': { method:'GET',isArray:false }
 		    });
-  }]);
+  }])
+	.factory('UserstoryBySprintId', ['$resource',
+		  function($resource){
+		    return $resource('http://localhost:8085/MUMScrum/API/UserStoryControllerWS/sprint/:sprintId/userstory', {}, {
+		      'query': { method:'GET',isArray:false }
+		    });
+  }])
+	.factory('NonSprintUserstory', ['$resource',
+		  function($resource){
+		    return $resource('http://localhost:8085/MUMScrum/API/UserStoryControllerWS/nonsprint/userstory', {}, {
+		      'query': { method:'GET',isArray:false }
+		    });
+  }])
